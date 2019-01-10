22cfb98f38a590a7c282d55a9748235153dfae41
Chuck Benedict <chuck@benedict.email>
2019-01-10 15:06:46 -0800

    Add some unit tests for CameraMount

--- src/test/java/org/team997coders/deepspace2019/subsystems/CameraMountUnitTest.java
+++ src/test/java/org/team997coders/deepspace2019/subsystems/CameraMountUnitTest.java
@@ -2,24 +2,84 @@ package org.team997coders.deepspace2019.subsystems;
 
 import org.junit.*;
 
-import org.team997coders.deepspace2019.interfaces.*;
+import org.team997coders.deepspace2019.helpers.TestLimitsHelper;
 
 import static org.junit.Assert.*;
 
-import static org.mockito.Mockito.*;
-
+/**
+ * Units tests for the CameraMount subsystem
+ */
 public class CameraMountUnitTest {
+
   @Test
   public void itCanDetectWhenAtLowerPanLimit() {
     // Assemble
-    IServo panServoMock = mock(IServo.class);
-    IServo tiltServoMock = mock(IServo.class);
-    CameraMount cameraMount = new CameraMount(panServoMock, tiltServoMock, 10, 180, 5, 180);
+    TestLimitsHelper helper = new TestLimitsHelper(10, 180, 5, 180);
 
     // Act
-    cameraMount.panToAngle(4);
+    helper.getCameraMount().panToAngle(4);
 
     // Assert
-    assertTrue(cameraMount.atPanLimit());
+    assertTrue(helper.getCameraMount().atPanLimit());
+  }
+
+  @Test
+  public void itCanDetectWhenAtUpperPanLimit() {
+    // Assemble
+    TestLimitsHelper helper = new TestLimitsHelper(10, 180, 5, 170);
+
+    // Act
+    helper.getCameraMount().panToAngle(180);
+
+    // Assert
+    assertTrue(helper.getCameraMount().atPanLimit());
+  }
+
+  @Test
+  public void itCanDetectWhenAtLowerTiltLimit() {
+    // Assemble
+    TestLimitsHelper helper = new TestLimitsHelper(10, 180, 5, 180);
+
+    // Act
+    helper.getCameraMount().tiltToAngle(4);
+
+    // Assert
+    assertTrue(helper.getCameraMount().atTiltLimit());
+  }
+
+  @Test
+  public void itCanDetectWhenAtUpperTiltLimit() {
+    // Assemble
+    TestLimitsHelper helper = new TestLimitsHelper(10, 170, 5, 170);
+
+    // Act
+    helper.getCameraMount().tiltToAngle(180);
+
+    // Assert
+    assertTrue(helper.getCameraMount().atTiltLimit());
+  }
+
+  @Test
+  public void itCanDetectWhenWithinPanLimit() {
+    // Assemble
+    TestLimitsHelper helper = new TestLimitsHelper(10, 180, 100, 150);
+
+    // Act
+    helper.getCameraMount().panToAngle(125);
+
+    // Assert
+    assertTrue(!helper.getCameraMount().atPanLimit());
+  }
+
+  @Test
+  public void itCanDetectWhenWithinTiltLimit() {
+    // Assemble
+    TestLimitsHelper helper = new TestLimitsHelper(10, 180, 100, 150);
+
+    // Act
+    helper.getCameraMount().tiltToAngle(100);
+
+    // Assert
+    assertTrue(!helper.getCameraMount().atTiltLimit());
   }
 }
